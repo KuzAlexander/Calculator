adf4f7e44fa996054ce2a8ce94227193c02f9a9a
Aleksandr <ptralex@mail.ru>
2021-08-27 01:48:21 +0300

    добавлены классы Request, Response

---
+++ web/core/Response.php
@@ -0,0 +1,26 @@
+<?php
+class Response
+{
+    private $content;
+
+    public function setStatusCode(int $code, string $phrase): void
+    {
+        header("HTTP/1.1 $code $phrase");
+    }
+
+    public function addHeader(string $name, string $value): void
+    {
+        header("$name: $value");
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function send(): void
+    {
+        echo $this->content;
+        exit;
+    }
+}
