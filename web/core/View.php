a67da4c32c2244d6b2eb9ac9268cbb2d2114a113
Aleksandr <ptralex@mail.ru>
2021-09-07 21:46:59 +0300

    В класс View добавлен метод render()

--- web/core/View.php
+++ web/core/View.php
@@ -2,11 +2,17 @@
 
 class View
 {
-    public function renderPhpFile(string $path, array $params = null): string
+    public function renderPhpFile(string $path, array $params = []): string
     {
         extract($params, EXTR_OVERWRITE);
         ob_start();
         require_once str_replace('@', dirname(__DIR__, 2), $path);
         return ob_get_clean();
+    }
+
+    public function render(string $content, string $path = '@/web/views/layouts/main.php'): string
+    {
+        $params['content'] = $content;
+        return $this->renderPhpFile($path, $params);
     }
 }
